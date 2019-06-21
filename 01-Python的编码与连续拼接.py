05d6566ae7c9b9e41be48b6ba14b3b7fc1cd8fbe
Neio <1126819198@qq.com>
2019-06-21 22:10:48 +0800

    Python基础

---
+++ 01-Python的编码与连续拼接.py
@@ -0,0 +1,7 @@
+# coding: UTF-8
+
+print ("hello,python,你的世界")
+# 2.7不能直接输出中文print ("你好世界"),需要在文件的第一行加UTF-8
+
+# Python中可以使用*来连续拼接字符串
+print ("_" * 50)
---
+++ 02-变量的定义.py
@@ -0,0 +1,23 @@
+# coding: UTF-8
+
+# 1.定义一个变量qq账号变量(""表示字符串 )
+qq_number = "12321131"
+# 2.定义一个变量记录qq密码
+qq_password = "123"
+# 输出需要使用print函数
+print (qq_password)
+
+# 超市买苹果的函数(Python写方法不需要格式,直接写就好)
+# 1.定义苹果的单价
+price = 8.5
+# 2.挑选苹果
+weight = 7.5
+# 3.计算付款金额
+money = weight * price
+# 3.5只要买苹果就返还5元,这里推出Python是单例的,不会创建新的对象
+money = money - 5
+# 4.输出
+print (money)
+
+
+
